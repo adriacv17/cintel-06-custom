d42f6580a1f05cdb81dde75d4496aa206e93ddc9
adriacv17 <142825332+adriacv17@users.noreply.github.com>
2024-04-06 20:56:28 -0500

    Commented Instructions

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
+# Imports
 
+# Set DEQUE
+
+# Reactive Aspects 
+
+# Define at least one reactive calc that can be called by your UI output components
+
+# Define Shiny UI page layout
+
+# Call the ui.page_opts() function
+
+# UI Page Inputs
+
+# UI Sidebar Components
+
+# Choose at least one input that the user can interact with to filter the data set
+
+# UI Main Content
+
+# UI Page Outputs
+
+# Everything not in the sidebar is the main content.
+# Will you use a template?
+# Will you use layout columns?
+# Will you use navigation or accordion components?
+# Define some output text. Will it be in a card? A value box? 
+# Define an output table or grid to show your filtered data set
+# Define an  output widget or chart (e.g., a Plotly Express chart) to show the filtered data graphically
